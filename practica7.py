bf01a4576f54aca16f3c9dc653104c75126a384c
revilla-92 <daniel.revilla.twose@gmail.com>
2015-12-30 17:16:29 +0100

    Small changes

--- practica7.py
+++ practica7.py
@@ -14,7 +14,7 @@ print("-----------------------------------------------------------------------")
 print("------------------------- Empieza el script ---------------------------")
 
 # Eliminamos si existe el directorio de trabajo para volver a importar los ficheros.
-if (not os.path.isdir("cdps")):
+if (os.path.isdir("cdps")):
 	os.system("rm -rf cdps")
 
 
@@ -26,41 +26,40 @@ os.system("tar xfvz p7.tgz")
 os.chdir("p7")
 os.system("rm -rf p7.xml")
 os.system("wget https://raw.githubusercontent.com/revilla-92/CDPSfy_MV/master/p7.xml")
-os.system("cd cdps/p7")
 
 
 print("-----------------------------------------------------------------------")
 print("---------------------- Empieza la configuracion -----------------------")
 
 # Como trabajamos en maquina virtual
-os.system("sudo su -c ./bin/prepare-p7-vm")
+os.system("sudo su -c './bin/prepare-p7-vm'")
 
 # Destruimos el escenario anterior si lo hubiese y creamos el nuevo
-os.system("sudo su -c vnx -f p7.xml -v --destroy")
-os.system("sudo su -c vnx -f p7.xml -v --create")
+os.system("sudo su -c 'vnx -f p7.xml -v --destroy'")
+os.system("sudo su -c 'vnx -f p7.xml -v --create'")
 
 
 print("-----------------------------------------------------------------------")
 print("------------------------- Configurando NAS ----------------------------")
 
-os.system("sudo su -c lxc-attach -n nas1 -- gluster peer probe 10.1.3.22")
-os.system("sudo su -c lxc-attach -n nas1 -- gluster peer probe 10.1.3.23")
-os.system("sudo su -c lxc-attach -n nas1 -- gluster volume create nas replica 3 10.1.3.21:/nas 10.1.3.22:/nas 10.1.3.23:/nas force")
-os.system("sudo su -c lxc-attach -n nas1 -- gluster volume start nas")
+os.system("sudo su -c 'lxc-attach -n nas1 -- gluster peer probe 10.1.3.22'")
+os.system("sudo su -c 'lxc-attach -n nas1 -- gluster peer probe 10.1.3.23'")
+os.system("sudo su -c 'lxc-attach -n nas1 -- gluster volume create nas replica 3 10.1.3.21:/nas 10.1.3.22:/nas 10.1.3.23:/nas force'")
+os.system("sudo su -c 'lxc-attach -n nas1 -- gluster volume start nas'")
 
 
 print("-----------------------------------------------------------------------")
 print("---------------------- Configurando Servidores ------------------------")
 
 for n in range(1, 5):
-	comando = "sudo su -c lxc-attach -n s"+str(n)+" -- mkdir /mnt/nas"
+	comando = "sudo su -c 'lxc-attach -n s"+str(n)+" -- mkdir /mnt/nas'"
 	os.system(comando)
 
 	if(n == 4):
-		comando1 = "sudo su -c lxc-attach -n s"+str(n)+" -- mount -t glusterfs 10.1.3."+str(n+19)+":/nas /mnt/nas"
+		comando1 = "sudo su -c 'lxc-attach -n s"+str(n)+" -- mount -t glusterfs 10.1.3."+str(n+19)+":/nas /mnt/nas'"
 		os.system(comando)
 	else:
-		comando1 = "sudo su -c lxc-attach -n s"+str(n)+" -- mount -t glusterfs 10.1.3."+str(n+20)+":/nas /mnt/nas"
+		comando1 = "sudo su -c 'lxc-attach -n s"+str(n)+" -- mount -t glusterfs 10.1.3."+str(n+20)+":/nas /mnt/nas'"
 		os.system(comando)
 
 
